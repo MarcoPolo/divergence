d2521f785be993824836b89cb2076fd9e3f28559
FaytxZen <andrewvora@ufl.edu>
2014-04-02 23:23:31 -0400

    Added some sounds to web and started bgm method.

--- src/divergence/audio.cljs
+++ src/divergence/audio.cljs
@@ -1,9 +1,20 @@
-(ns divergence.audio)
+(ns divergence.audio
+  :require [divergence.system :as s]
+  )
 
 (def jumpSound (js/buzz.sound. "assets/sounds/fins__jumping.wav"))
+(def pickUpSound (js/buzz.sound. "assets/sounds/adriancalzon__pickup1.mp3"))
+(def pushSound (js/buzz.sound. "assets/sounds/stanestane__push.wav"))
+(def bgm1 (js/buzz.sound. "Mellowtron.mp3"))
+(def bgm2 (js/buzz.sound. "Pamgaea.mp3"))
 
 (def sounds {
-             :jump jumpSound
+             :jump    jumpSound
+             :pickup  pickUpSound
+             :push    pushSound
+
+             :bgm1    bgm1
+             :bgm2    bgm2
              })
 
 (defn play-sound [key]
@@ -11,3 +22,10 @@
     (. sound load)
     (. sound play)
   ))
+
+;;use this in core, may need adjustments
+(defn startBGM []
+  (case (deref s/level)
+    0 (play-sound :bgm1)
+    1 (play-sound :bgm2)
+    ))
