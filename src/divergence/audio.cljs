70d98f5547e66bec053838abf0e2af49a2205f2c
FaytxZen <andrewvora@ufl.edu>
2014-04-13 03:00:01 -0400

    Added new sound. Added to audio.cljs

--- src/divergence/audio.cljs
+++ src/divergence/audio.cljs
@@ -1,20 +1,21 @@
 (ns divergence.audio
-  ;(:require [divergence.system :as s])
   )
 
 (def jumpSound (js/buzz.sound. "assets/sounds/fins__jumping.wav"))
 (def pickUpSound (js/buzz.sound. "assets/sounds/adriancalzon__pickup1.mp3"))
 (def pushSound (js/buzz.sound. "assets/sounds/stanestane__push.wav"))
+(def magicSound (js/buzz.sound. "assets/joe93barlow__protego.mp3"))
 (def bgm1 (js/buzz.sound. "Mellowtron.mp3"))
 (def bgm2 (js/buzz.sound. "Pamgaea.mp3"))
 
 (def sounds {
-             :jump    jumpSound
-             :pickup  pickUpSound
-             :push    pushSound
+             :jump      jumpSound
+             :pickup    pickUpSound
+             :push      pushSound
+             :teleport  magicSound
 
-             :bgm1    bgm1
-             :bgm2    bgm2
+             :bgm1      bgm1
+             :bgm2      bgm2
              })
 
 (defn play-sound [key]
